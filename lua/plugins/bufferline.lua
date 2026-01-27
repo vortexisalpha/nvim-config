843ef370f4d25b32c98565d638b39ef3630181ae
vortexisalpha <joshuaboshua250505@gmail.com>
2026-01-27 09:16:27 -0600

    add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+require("bufferline").setup({
+  options = {
+    mode = "buffers",
+    diagnostics = "nvim_lsp",
+    always_show_bufferline = false,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    separator_style = "slant",
+
+  },
+})
+
